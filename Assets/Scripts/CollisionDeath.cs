f4a2c778b04634570e983616353db41811e9fcf6
Mateus A <mam56.art@gmail.com>
2020-05-06 17:19:01 -0300

    Colisão MC
    Script da Colisão do MC com os obstaculos
    Tag usada: "Obstaculo"

---
+++ Assets/Scripts/CollisionDeath.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDeath : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Obstaculo")
+        {
+            Debug.Log("Game Over");
+        }
+            
+    }
+        
+
+}
