0566511c369b9e1d0afc3e6fe92988d9cec7ef03
Mateus A <mam56.art@gmail.com>
2020-05-06 16:29:54 -0300

    Script Pulo
    Script do Pulo do personagem Principal

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public Vector2 JumpForce;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            GetComponent<Rigidbody2D>().AddForce(JumpForce);
+    }
+}
