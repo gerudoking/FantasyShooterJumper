e309c21be0f2a6386426289eb1c1d764f06a91a9
Mateus A <mam56.art@gmail.com>
2020-05-13 16:26:09 -0300

    Colisão Versão Atualizada
    Script da colisão atualizado com uma função de "LoadScene"

---
+++ Assets/Scripts/CollisionDeathV2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionDeathV2 : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Obstaculo")
+        {
+            Debug.Log("Game Over");
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    private void Update()
+    {
+        if (transform.position.y > 5.5)
+            SceneManager.LoadScene(0);
+        if (transform.position.y < -4.2)
+            SceneManager.LoadScene(0);
+    }
+
+
+}
