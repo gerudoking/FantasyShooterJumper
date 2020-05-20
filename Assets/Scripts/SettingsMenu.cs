c539c309fb9f72575533ff7cc3b102294319f192
Mateus A <mam56.art@gmail.com>
2020-05-20 16:48:32 -0300

    Animação MC, Scripts e Scenes de Menu
    Animação do MC:
    - Projétil
    -Voo do MC
    -Braço durante voo
    -Braço durante Ataque
    Scripts:
    -Scripts do Menu
    -Scripts dos Settings
    -Scripts do GameOver
    Scenes:
    -GameOver
    -Main Menu

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+
+    public AudioMixer audioMixer;
+
+    public void SetMusic(float volume)
+    {
+        audioMixer.SetFloat("music", volume);
+    }
+    public void SetSFX(float volume)
+    {
+        audioMixer.SetFloat("sfx", volume);
+    }
+
+}
