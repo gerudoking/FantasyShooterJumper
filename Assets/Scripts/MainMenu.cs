9fa4c4d270548451f7397509825cea3463aa9b0c
gerudoking <edsantos2003@gmail.com>
2020-05-24 11:25:48 -0300

    Teste do menu em progresso

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,10 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private BaseHorizontalValue value = null;
+    [SerializeField] private Transform gameObjects = null;
+    public static int gameState = 0;
 
     public void PlayGame ()
     {
-        SceneManager.LoadScene(1);
+        value.speed *= 2;
+        gameState = 1;
+        gameObjects.gameObject.SetActive(true);
+
+        foreach(Transform t in transform) {
+            t.gameObject.SetActive(false);
+        }
     }
 
     public void QuitGame()
