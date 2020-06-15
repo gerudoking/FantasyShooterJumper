e447c2ca75e4b49725b32063e23ff3367eb1a4be
Mateus A <mam56.art@gmail.com>
2020-06-15 16:21:05 -0300

    Escudo

---
+++ Assets/Scripts/Mateus/ShieldDestroy.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldDestroy : MonoBehaviour
+{
+
+    public Material ShieldDissolve;
+    public Material Bloom;
+    private bool isDissolving = false;
+    private float fade = 1.0f;
+    public GameObject Shield2;
+    void Start()
+    {
+        Shield2.SetActive(false);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Obstaculo")
+        {
+            Debug.Log("GameOver");
+                StartDestroy();
+        }
+    }
+
+
+    private void StartDestroy()
+    {
+        isDissolving = true;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GetComponent<SpriteRenderer>().material = Bloom;
+            isDissolving = false;
+            fade = 1.0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+            StartDestroy();
+
+   
+        if (isDissolving)
+        {
+            fade -= Time.deltaTime * 1.5f;
+
+            if (fade <= 0.0f)
+            {
+                fade = 0.0f;
+                Shield2.SetActive(false);
+            }
+            ShieldDissolve = GetComponent<SpriteRenderer>().material = ShieldDissolve;
+            ShieldDissolve.SetFloat("_Fade", fade);
+        }
+    }
+
+}
